a6a67651db1da811a4d11c362333e73c0570c1b2
M-ELGHANDOUR <mohsinelghandour20@gmail.com>
2026-04-02 10:27:38 +0200

    Delete src directory

--- src/data_engineering/__init__.py
+++
@@ -1,6 +0,0 @@
-# Data Engineering — Chlordécone Project
-from .ingestion import load_raw, validate_schema, quick_report
-from .cleaning  import clean
-from .pipeline  import run_pipeline
-
-__all__ = ['load_raw', 'validate_schema', 'quick_report', 'clean', 'run_pipeline']
